20ea91522f273be6a95c3004829ee5481ecdf9ec
Kaden Seeger <02kaden.seeger@gulllakecs.org>
2019-02-14 09:34:44 -0500

    Add Functions and Variables

--- Functions-Git.py
+++ Functions-Git.py
@@ -1,3 +1,4 @@
+'''
 #2.13.3: Greeting
 #Kaden Seeger
 #2/6/19
@@ -9,3 +10,17 @@ def greeting():
     print("Hi there " + name + "!")
     print( "nice to meet you ")
 greeting()
+'''
+
+#4.13.4: Functions and variables '
+#Kaden Seeger
+#2.14.19
+
+
+x = 406
+
+def print_somthing():
+    x = 12
+    print(x)
+print_somthing()
+print(x)
