7aa8fdb910ca7727cf700b6754e70cf37ed3618d
Kaden Seeger <02kaden.seeger@gulllakecs.org>
2019-02-14 09:12:09 -0500

    Add a greeting Function

---
+++ Functions-Git.py
@@ -0,0 +1,11 @@
+#2.13.3: Greeting
+#Kaden Seeger
+#2/6/19
+
+
+name = input("What is your name:")
+
+def greeting():
+    print("Hi there " + name + "!")
+    print( "nice to meet you ")
+greeting()
